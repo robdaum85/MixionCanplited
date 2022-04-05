fa93d8b207ceb703e37037d63ff8f117bbb800d6
Robson Santos Oliveira Junior <41492470+robdaum85@users.noreply.github.com>
2022-04-04 21:40:07 -0300

    Deploy in netlify
    the site is on in https://robdaum85.netlify.app/

---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+    require 'teste.php';
+
+    $c = new Caneta;
+    $c->cor = "Azul";
+    $c->modelo = "Bic";
+    https://www.youtube.com/watch?v=B55Vs5-u-gQ
+    //var_dump($c);
+echo var_dump($c);

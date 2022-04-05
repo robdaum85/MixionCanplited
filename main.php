027be17ef437d08918013da4c6956f3b6216f171
Robson Santos Oliveira Junior <41492470+robdaum85@users.noreply.github.com>
2022-04-04 21:52:23 -0300

    Delete main.php

--- main.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require 'teste.php';
-
-    $c = new Caneta;
-    $c->cor = "Azul";
-    $c->modelo = "Bic";
-    https://www.youtube.com/watch?v=B55Vs5-u-gQ
-    //var_dump($c);
-echo var_dump($c);
